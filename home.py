bf8dcf2f390c52459e3dd6f269bb5db8570f373a
Satyajit Shil <satyajitshil02@gmail.com>
2025-07-04 11:23:31 -0700

    homework

---
+++ home.py
@@ -0,0 +1,5 @@
+age = int(input("How old are you:"))
+if 10 <= age <= 20:
+    print("You are between 10 and 20 years old, inclusive")
+else:
+    print("You are not between 10 and 20 years old, inclusive")
